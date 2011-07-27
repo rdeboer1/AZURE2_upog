84c9815e5469098a9f093c93b32b2f0441c270d2
Ethan Uberseder <eubersed@eubersed-desktop.(none)>
2011-07-27 12:26:23 -0400

    Added absorbtion of mouse down/move events on the first two columns of the level view.  Previously just absorbed mouse up events. Updated README.

--- gui/include/LevelsHeaderView.h
+++ gui/include/LevelsHeaderView.h
@@ -10,6 +10,16 @@ class LevelsHeaderView : public QHeaderView {
       setClickable(true);
     };
  protected:
+   virtual void mouseMoveEvent ( QMouseEvent * e ) {
+     int pos = orientation() == Qt::Horizontal ? e->x() : e->y();
+     int section = logicalIndexAt(pos);
+     if(section>1) QHeaderView::mouseMoveEvent(e);
+    };
+   virtual void mousePressEvent ( QMouseEvent * e ) {
+     int pos = orientation() == Qt::Horizontal ? e->x() : e->y();
+     int section = logicalIndexAt(pos);
+     if(section>1) QHeaderView::mousePressEvent(e);
+    };
    virtual void mouseReleaseEvent ( QMouseEvent * e ) {
      int pos = orientation() == Qt::Horizontal ? e->x() : e->y();
      int section = logicalIndexAt(pos);
