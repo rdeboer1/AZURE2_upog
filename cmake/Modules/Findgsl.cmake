ba35a619f93a991efa85b1b9024ddbcaa7597d9d
Richard J deBoer <rdeboer1@regulus.crc.nd.edu>
2017-01-24 10:30:58 -0500

    made a few path updates to cmake find files

--- cmake/Modules/Findgsl.cmake
+++ cmake/Modules/Findgsl.cmake
@@ -1,11 +1,19 @@
+#Note, the path to GSL also needs to be defined so that it can find the shared library files at run time. This has been done in my .login file by the command
+#setenv LD_LIBRARY_PATH ${LD_LIBRARY_PATH}:/opt/crc/scilib/gsl/1.15/gnu-4.6.2/lib
+#Of course if I change versions of gsl I will need to update this both here and in my .login file
+
+
 find_path(GSL_INCLUDE_DIR gsl/gsl_cblas.h
           HINTS $ENV{HOME}/local/include $ENV{HOME}/include /opt/crc/scilib/gsl/1.15/gnu-4.6.2/include)
+# /opt/crc/scilib/gsl/1.15/gnu-4.6.2/include
 
 find_library(GSL_LIBRARY NAMES gsl 
              HINTS $ENV{HOME}/local/lib $ENV{HOME}/lib /opt/crc/scilib/gsl/1.15/gnu-4.6.2/lib)
+# /opt/crc/scilib/gsl/1.15/gnu-4.6.2/lib
 
 find_library(GSLCBLAS_LIBRARY NAMES gslcblas 
              HINTS $ENV{HOME}/local/lib $ENV{HOME}/lib /opt/crc/scilib/gsl/1.15/gnu-4.6.2/lib)
+# /opt/crc/scilib/gsl/1.15/gnu-4.6.2/lib
 
 set(GSL_LIBRARIES ${GSL_LIBRARY} ${GSLCBLAS_LIBRARY})
 
