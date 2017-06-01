4c8f1faacf91d5fac1dd6f22b41b817a58dcfa88
Richard J deBoer <rdeboer1@regulus.crc.nd.edu>
2017-06-01 16:36:05 -0400

    minor change to cmake find file

--- cmake/Modules/Findgsl.cmake
+++ cmake/Modules/Findgsl.cmake
@@ -4,15 +4,15 @@
 
 
 find_path(GSL_INCLUDE_DIR gsl/gsl_cblas.h
-          HINTS $ENV{HOME}/local/include $ENV{HOME}/include /opt/crc/scilib/gsl/1.15/gnu-4.6.2/include)
+          HINTS $ENV{HOME}/local/include $ENV{HOME}/include /opt/crc/scilib/gsl/1.15/gnu-4.6.2/include) # /opt/crc/scilib/gsl/1.15/gnu-4.6.2/include
 # /opt/crc/scilib/gsl/1.15/gnu-4.6.2/include
 
 find_library(GSL_LIBRARY NAMES gsl 
-             HINTS $ENV{HOME}/local/lib $ENV{HOME}/lib /opt/crc/scilib/gsl/1.15/gnu-4.6.2/lib)
+             HINTS $ENV{HOME}/local/lib $ENV{HOME}/lib /opt/crc/scilib/gsl/1.15/gnu-4.6.2/lib) # /opt/crc/scilib/gsl/1.15/gnu-4.6.2/lib
 # /opt/crc/scilib/gsl/1.15/gnu-4.6.2/lib
 
 find_library(GSLCBLAS_LIBRARY NAMES gslcblas 
-             HINTS $ENV{HOME}/local/lib $ENV{HOME}/lib /opt/crc/scilib/gsl/1.15/gnu-4.6.2/lib)
+             HINTS $ENV{HOME}/local/lib $ENV{HOME}/lib /opt/crc/scilib/gsl/1.15/gnu-4.6.2/lib) # /opt/crc/scilib/gsl/1.15/gnu-4.6.2/lib
 # /opt/crc/scilib/gsl/1.15/gnu-4.6.2/lib
 
 set(GSL_LIBRARIES ${GSL_LIBRARY} ${GSLCBLAS_LIBRARY})
