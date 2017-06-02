8278c53df12fe03762c9992bdb71d4a9e004b7c7
Richard J deBoer <rdeboer1@canopus.crc.nd.edu>
2017-06-02 11:52:34 -0400

    updated cmake gsl find file with environment path and changed default to QWT=ON in main cmake file

--- cmake/Modules/Findgsl.cmake
+++ cmake/Modules/Findgsl.cmake
@@ -1,18 +1,15 @@
-#Note, the path to GSL also needs to be defined so that it can find the shared library files at run time. This has been done in my .login file by the command
-#setenv LD_LIBRARY_PATH ${LD_LIBRARY_PATH}:/opt/crc/scilib/gsl/1.15/gnu-4.6.2/lib
-#Of course if I change versions of gsl I will need to update this both here and in my .login file
-
+#It looks like a GSL environment variable is now available GSL_DIR, so I am using this to elimiate the hard coded path
 
 find_path(GSL_INCLUDE_DIR gsl/gsl_cblas.h
-          HINTS $ENV{HOME}/local/include $ENV{HOME}/include /opt/crc/scilib/gsl/1.15/gnu-4.6.2/include) # /opt/crc/scilib/gsl/1.15/gnu-4.6.2/include
+          HINTS $ENV{HOME}/local/include $ENV{HOME}/include $ENV{GSL_DIR}/include) # /opt/crc/scilib/gsl/1.15/gnu-4.6.2/include
 # /opt/crc/scilib/gsl/1.15/gnu-4.6.2/include
 
 find_library(GSL_LIBRARY NAMES gsl 
-             HINTS $ENV{HOME}/local/lib $ENV{HOME}/lib /opt/crc/scilib/gsl/1.15/gnu-4.6.2/lib) # /opt/crc/scilib/gsl/1.15/gnu-4.6.2/lib
+             HINTS $ENV{HOME}/local/lib $ENV{HOME}/lib $ENV{GSL_DIR}/lib) # /opt/crc/scilib/gsl/1.15/gnu-4.6.2/lib
 # /opt/crc/scilib/gsl/1.15/gnu-4.6.2/lib
 
 find_library(GSLCBLAS_LIBRARY NAMES gslcblas 
-             HINTS $ENV{HOME}/local/lib $ENV{HOME}/lib /opt/crc/scilib/gsl/1.15/gnu-4.6.2/lib) # /opt/crc/scilib/gsl/1.15/gnu-4.6.2/lib
+             HINTS $ENV{HOME}/local/lib $ENV{HOME}/lib $ENV{GSL_DIR}/lib) # /opt/crc/scilib/gsl/1.15/gnu-4.6.2/lib
 # /opt/crc/scilib/gsl/1.15/gnu-4.6.2/lib
 
 set(GSL_LIBRARIES ${GSL_LIBRARY} ${GSLCBLAS_LIBRARY})
